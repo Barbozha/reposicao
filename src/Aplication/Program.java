25889f3efc78247af40dcfd7f0a682c6bcb3cd3e
Barboza <barbozha@gmail.com>
2021-03-17 16:23:28 -0300

    ALteracao1 do projeto

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,19 +1,25 @@
 package Aplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
-public class Program {
+import entity.Cliente;
 
+public class Program {
 	public static void main(String[] args) {
 		// Criação de um Menu
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		Cliente cliente = new Cliente();
 		int opc = 1;
 		while(opc != 3) {
 			opc = criarMenu(sc);
 			if(opc == 1) {
-				//consultar(sc, agenda);
+				cadastroCliente(sc,cliente);
 			}else if(opc == 2){
-				
+				listaCadastro(sc);
 			}else if(opc == 3){
 				System.out.println("Você saiu do Sistema!");
 				System.exit(0);
@@ -27,8 +33,7 @@ public class Program {
 	public static int criarMenu(Scanner sc) {
 		int opcao = 3;
 		boolean valida = false;
-		while(!valida) {
-			
+		while(!valida) {			
 			System.out.println("*--------------------*");
 			System.out.println("|   Menu de Opções   |");
 			System.out.println("|---------------------");
@@ -38,8 +43,8 @@ public class Program {
 			System.out.println("*--------------------*");
 			System.out.print("Escolha opção: ");
 			try {
-				String entrada = sc.nextLine();
-				opcao = Integer.parseInt(entrada);
+				//String entrada = sc.nextLine();
+				opcao = sc.nextInt();
 				if(opcao == 1 || opcao == 2 || opcao == 3) {
 					valida = true;
 				}else {
@@ -51,6 +56,40 @@ public class Program {
 			}	
 		}
 		return opcao;
+		
+	}
+	
+	public static void cadastroCliente(Scanner sc, Cliente cliente) {
+		System.out.println("Cadastro de Clientes");
+		System.out.print("Quantos clientes a cadastrar? ");
+		int id;
+		String nome;
+		String email;
+		String telefone;
+		int num = sc.nextInt();
+		ArrayList<Cliente> lista = new ArrayList<Cliente>();
+		for(int i = 1; i <= num; i++) {
+			System.out.println(i+"o Cliente");
+			System.out.print("Informe o Id: ");
+			id = sc.nextInt();
+			System.out.print("Informe o nome: ");
+			sc.nextLine();
+			nome = sc.nextLine();
+			System.out.print("Informe o e-mail: ");
+			email = sc.nextLine();
+			System.out.print("Informe o telefone: ");
+			telefone = sc.next();
+			lista.add(new Cliente(id,nome,email,telefone));
+		}
+	}
+	
+	public static void listaCadastro(Scanner sc) {
+		System.out.println("--- Listagem de Clientes ---");
+		ArrayList<Cliente> lista = new ArrayList<Cliente>();
+		ArrayList< Cliente > a1 = new ArrayList< Cliente >();
+		a1.addAll(lista);
+		System.out.println(a1);
+			//return "Id: "+lista.getId(0)+", Nome: "+c.getNome()+", E-mail: "+c.getEmail()+", Telefone: "+c.getTelefone();
 		
 	}
 }
